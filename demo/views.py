e55fd4f8752b3dccce8cd7913bb65c521bb38b70
Dino <dino.osmanovic@symphony.is>
2017-05-22 10:57:04 +0200

    feature demo2 branch created and added

--- demo/views.py
+++ demo/views.py
@@ -11,4 +11,5 @@ def index(request):
             Outputs html response.
 
         """
-    return HttpResponse("Hey Dude - you did it, greatings from Symphony Crew!")
+    return HttpResponse("Hey Dude - you did it - you managed to run demo2 branch!! "
+                        " Once again, greatings from Symphony Crew!")
